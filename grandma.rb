54c175b1efa98de4f797c0c3d76844b8d78940ad
Joshaharonoff <josh.aharonoff@gmail.com>
2015-07-22 00:19:12 -0400

    3rd attempt

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(text)
     if text==text.upcase
-        return "NO,NOT SINCE 1938!"
+        return "NO, NOT SINCE 1938!"
         else
-        return "HUH?! SPEAK UP,SONNY!"
+        return 'HUH?! SPEAK UP, SONNY!'
     end
 end
 
