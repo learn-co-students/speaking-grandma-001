418c282182dbaeeb0cadf58f518b9049f6f6cc1f
Chris Aporta <christopher.aporta@gmail.com>
2015-08-17 20:21:51 -0400

    init commit

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,11 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(greeting)
+  if greeting == greeting.upcase
+    "No, not since 1938!".upcase
+  else
+    "Huh?! Speak up, sonny!".upcase
+  end
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
@@ -8,3 +14,5 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+speak_to_grandma("WHAT DID YOU EAT TODAY?")
